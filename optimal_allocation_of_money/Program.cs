f33dbe71b4ff5edac9f9ba6dcdbf30ec67460efd
BaX <chernoff.serg3i@yandex.ru>
2024-05-23 20:48:34 +0300

    start created

--- optimal_allocation_of_money/Program.cs
+++ optimal_allocation_of_money/Program.cs
@@ -2,9 +2,15 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать! Это программа для расчёта оптимального вклада в банк.");
+            Console.WriteLine("Данные о количестве вклада и сумме будут взяты из файла программы.");
+            List<int> numberbank = new List<int>();
+            List<int> temp = new List<int>();
+            List<int> money = new List<int>();
+            List<List<int>> matrix = new List<List<int>>();
+
         }
     }
 }
---
+++ optimal_allocation_of_money/method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optimal_allocation_of_money
+{
+    class method
+    {
+        public List<int> numberbank = new List<int>();
+        public List<int> temp = new List<int>();
+        public List<int> money = new List<int>();
+        public List<List<int>> matrix = new List<List<int>>();
+
+        void GetDate()
+        {
+
+        }
+    }
+}

674f2d6004878c7c775c35f32349f7b458fad1bb
BaX <chernoff.serg3i@yandex.ru>
2024-05-23 17:20:28 +0300

    Add project files.

---
+++ optimal_allocation_of_money/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace optimal_allocation_of_money
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

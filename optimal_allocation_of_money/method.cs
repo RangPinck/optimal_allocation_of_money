6b7989af8ba05a0e24af8ac6f67cb034aed4704e
BaX <chernoff.serg3i@yandex.ru>
2024-05-25 22:33:15 +0300

    create gets data

--- optimal_allocation_of_money/method.cs
+++ optimal_allocation_of_money/method.cs
@@ -5,15 +5,47 @@
         string pathIn;
         string pathOut;
 
-        List<int> numberbank = new List<int>();
-        List<int> moneyStep = new List<int>();
-        int CostStep;
-        List<List<int>> matrix = new List<List<int>>();
+        int? CostStep;
+        List<List<int?>> matrix = new List<List<int?>>();
+        //
         List<int> StoryOptimaise = new List<int>();
 
-        void GetDate(string pathIn)
+        public method(string pathIn, string pathOut)
         {
+            this.pathIn = pathIn;
+            this.pathOut = pathOut;
 
+            GetDate();
+
+        }
+
+        void GetDate()
+        {
+            using StreamReader sr = new StreamReader(pathIn);
+
+            string line;
+
+            List<int?> temp = new List<int?>();
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                foreach (var item in line.Split('\t'))
+                {
+                    if (item == "")
+                    {
+                        temp.Add(null);
+                        continue;
+                    }
+
+                    temp.Add(int.Parse(item));
+                }
+
+                matrix.Add(new List<int?>(temp));
+
+                temp.Clear();
+            }
+
+            CostStep = matrix[1][2] - matrix[1][1];
         }
     }
 }

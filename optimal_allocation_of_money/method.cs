806d44fc232fa22605589f881103cd6c3261177c
BaX <chernoff.serg3i@yandex.ru>
2024-05-26 15:41:53 +0300

    end task

--- optimal_allocation_of_money/method.cs
+++ optimal_allocation_of_money/method.cs
@@ -11,23 +11,19 @@ namespace optimal_allocation_of_money
         //0 - статусы; i - сумма процентов; i+1 - индексация максимальной суммы (1 - сумма максимальная; null - нет)
         List<List<int?>> StoryOptimaise = new List<List<int?>>();
         List<int?> moneyInBank = new List<int?>();
-        int? finction { get; set; }
+        int? function { get; set; }
         int indexWorkingBank { get; set; }
-
         public method(string pathIn, string pathOut)
         {
             this.pathIn = pathIn;
             this.pathOut = pathOut;
-
             GetDate();
-
             CreateStoryListMarcup();
-
             CreateStory();
-
+            GetResults();
+            GetFunction();
             PostData();
         }
-
         void GetDate()
         {
             using StreamReader sr = new StreamReader(pathIn);
@@ -51,18 +47,16 @@ namespace optimal_allocation_of_money
             CostStep = matrix[2][0] - matrix[1][0];
             indexWorkingBank = matrix[0].Count - 1;
         }
-
         void PostData()
         {
             using StreamWriter sw = new StreamWriter(pathOut, false, Encoding.UTF8);
             sw.WriteLine("Банки:");
             for (int i = 0; i < moneyInBank.Count; i++)
             {
-                Console.WriteLine($"{i + 1}\t=\t{moneyInBank[i]}");
+                sw.WriteLine($"{i + 1}\t=\t{moneyInBank[i]}");
             }
-            sw.Write($"Максимальная прибыль = {finction}%");
+            sw.Write($"Максимальная прибыль = {function}%");
         }
-
         void CreateStoryListMarcup()
         {
             List<int?> temp = new List<int?>();
@@ -75,10 +69,8 @@ namespace optimal_allocation_of_money
             }
             StoryOptimaise.Add(new List<int?>(temp));
         }
-
         void CreateStory()
         {
-
             if (StoryOptimaise.Count == 1)
             {
                 SumProcentFirst();
@@ -89,17 +81,9 @@ namespace optimal_allocation_of_money
                 SumProcent();
                 indexWorkingBank--;
             }
-
-            GetMaxInCategory();
-
-            if (indexWorkingBank == 0)
-            {
-                
-            }
-
+            if (indexWorkingBank == 0) return;
             CreateStory();
         }
-
         void SumProcentFirst()
         {
             int CountSteps = 1;
@@ -118,10 +102,8 @@ namespace optimal_allocation_of_money
 
             StoryOptimaise.Add(new List<int?>(temp));
         }
-
         int? GetProcentContribution(int? indexBanc, int? costContribution)
         => matrix[index: (int)costContribution][index: (int)indexBanc];
-
         void SumProcent()
         {
             int CountSteps = 1;
@@ -142,44 +124,44 @@ namespace optimal_allocation_of_money
 
             StoryOptimaise.Add(new List<int?>(temp));
         }
-
         int? GetProcentContributionFromStory(int? contribytionInStory)
         {
             if (contribytionInStory == 0) return 0;
-
             int? CountElementsInCategory = 1 + contribytionInStory / CostStep;
             int? indexStartCategory = 0;
-
             for (int i = 2; i < CountElementsInCategory; i++)
             {
                 indexStartCategory += i;
             }
-
-            return StoryOptimaise[StoryOptimaise.Count - 2].GetRange((int)indexStartCategory, (int)CountElementsInCategory).Max();
+            return StoryOptimaise[StoryOptimaise.Count - 1].GetRange((int)indexStartCategory, (int)CountElementsInCategory).Max();
         }
-
-        void GetMaxInCategory()
+        void GetResults()
         {
-            List<int?> temp = new List<int?>();
-            int? maxInCategory = 0;
-            int indexStartCategory = 0;
-            int CountElementsInCategory = 2;
-            for (int i = 0; i < matrix.Count - 2; i++)
+            int? CountElementsInCategory = 1 + StoryOptimaise[0].Max() / CostStep;
+            int? indexStartCategory = 0;
+            int? max = 0;
+            List<int?> temp;
+            for (int i = StoryOptimaise.Count-1; i > 0; i--)
             {
-                maxInCategory = StoryOptimaise[StoryOptimaise.Count - 1].GetRange(indexStartCategory, CountElementsInCategory).Max();
-                for (int j = indexStartCategory; j < indexStartCategory+CountElementsInCategory; j++)
+                for (int j = 2; j < CountElementsInCategory; j++)
                 {
-                    if (StoryOptimaise[StoryOptimaise.Count - 1][j] == maxInCategory)
-                    {
-                        temp.Add(1);
-                        continue;
-                    }
-                    temp.Add(null);
+                    indexStartCategory += j;
                 }
-                indexStartCategory += CountElementsInCategory;
-                CountElementsInCategory++;
+                temp = new List<int?>(StoryOptimaise[i].GetRange((int)indexStartCategory, (int)CountElementsInCategory).ToList());
+                max = temp.Max();
+                moneyInBank.Add(temp.IndexOf(max)*CostStep);
+                CountElementsInCategory = (CountElementsInCategory - temp.IndexOf(max));
+                indexStartCategory = 0;
             }
-            StoryOptimaise.Add(new List<int?>(temp));
+            moneyInBank.Add(matrix[(int)CountElementsInCategory][0]);
+        }
+        void GetFunction()
+        {
+            function = 0;
+            for (int i = 0; i < moneyInBank.Count; i++)
+            {
+                function += matrix[(int)(moneyInBank[i] / CostStep + 1)][i+1];
+            }
         }
     }
 }
